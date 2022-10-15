a22d138e8b0f44aa87a9b1167c604527355c1650
Toru Ogawa <Hakuyume@users.noreply.github.com>
2022-10-15 10:38:36 +0900

    trace to stderr

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod mfa;
 mod storage;
 
 use clap::{Parser, Subcommand};
+use std::io;
+use tracing_subscriber::EnvFilter;
 
 #[derive(Parser)]
 struct Opts {
@@ -16,7 +18,10 @@ enum Command {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(io::stderr)
+        .init();
 
     let opts = Opts::parse();
     match opts.command {
